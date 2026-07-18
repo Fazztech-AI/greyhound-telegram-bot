e92c3553309382b751fe6b964c11a7a7ab6d48cd
Fazztech-AI <297722120+Fazztech-AI@users.noreply.github.com>
2026-07-18 10:40:36 +1000

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TOPAZ_API_KEY = os.getenv("TOPAZ_API_KEY")
 
+BETFAIR_APP_KEY = os.getenv("BETFAIR_APP_KEY")
+BETFAIR_SESSION_TOKEN = os.getenv("BETFAIR_SESSION_TOKEN")
+
 AUTHORITY_CODES = ["NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"]
